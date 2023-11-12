b18cb0f5dee6ef30b4fe4e2b772c4cfa9aeaca57
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2023-11-12 16:32:09 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -2,10 +2,8 @@
 #include <vector>
 #include <sstream>
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
-
 
 string solution(string s) 
 {
@@ -13,25 +11,21 @@ string solution(string s)
     std::vector<int> nums;
     
     int num;
-    int minnum;
-    int maxnum;
-
+    
     while(is >> num)
     {
         nums.push_back(num);
     }
     
-
-    
     if(!nums.empty())
     {
-         maxnum = *max_element(nums.begin(), nums.end());
-         minnum = *min_element(nums.begin(), nums.end());
+        int maxnum = *max_element(nums.begin(), nums.end());
+        int minnum = *min_element(nums.begin(), nums.end());
     }
     
   
     string answer = "";
     
-    answer = to_string(minnum) + " " + to_string(maxnum);
+    answer = to_string(maxnum) + " " + to_string(minnum);
     return answer;
 }
