0cdf4a3e56852c18ecbf6f2da42757fd734267f0
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2023-11-20 14:43:57 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.08 ms, Memory: 6.45 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<string>
+#include <stack>
+
+using namespace std;
+
+int solution(string s) 
+{
+    stack<char> st;
+
+    for (char c : s) 
+    {
+        if (!st.empty() && st.top() == c) 
+        {
+            st.pop(); 
+        }
+        else 
+        {
+            st.push(c); 
+        }
+    }
+
+    return st.empty() ? 1 : 0; 
+}
