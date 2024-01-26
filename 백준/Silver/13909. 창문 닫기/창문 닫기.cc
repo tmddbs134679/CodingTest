97ba0929dba0525cc0bf403c8b6cff833fd1bd85
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2024-01-26 19:36:36 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <unordered_set>
+#include <bitset>
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+    int N;
+    cin >> N;
+
+  
+    int result = static_cast<int>(std::sqrt(N));
+
+   cout << result;
+
+    return 0;
+}
