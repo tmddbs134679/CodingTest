074cea8c0dd0d16af0cc491c53eb6fae65de4627
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2024-04-15 22:22:41 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num)
+{
+	
+
+	if (num % 2 == 0)
+	{
+		return "Even";
+	}
+	else
+	{
+		return "Odd";
+	}
+
+	
+}
