48b33bd2e5eba698a937afb933c6a3aa4befd139
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2024-08-15 22:46:23 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -1,33 +1,22 @@
-
-
-#include <iostream>
 #include <string>
 #include <vector>
-#include <unordered_map>
-#include <map>
 #include <algorithm>
-#include <queue>
+
 using namespace std;
+
 vector<int> solution(vector<int> array, vector<vector<int>> commands) 
 {
     vector<int> answer;
-
-    for(auto test : commands)
+    
+    for(auto cm : commands)
     {
-        int start = test[0];
-        int end = test[1];
-
-        vector<int> newarr(array.begin() + start - 1, array.begin() + end);
-
-        sort(newarr.begin(), newarr.end());
-
-
+        vector<int> newans(array.begin() + cm[0] -1 , array.begin() + cm[1]);
         
-        answer.push_back(newarr[test[2] - 1]);
-
+        sort(newans.begin(), newans.end());
+        
+        answer.push_back(newans[cm[2] - 1]);
     }
-
-
-
+    
+    
     return answer;
 }
