9ec31922faeb9332368f925b159769b539bd93ec
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2024-08-22 13:21:17 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) 
+{
+       if (n <= 1) return 1;
+    
+    vector<long long> dp(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+    
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+    }
+    
+    return dp[n];
+}
